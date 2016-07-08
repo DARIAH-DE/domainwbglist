3fea8e1cf31f95fa4759a02b477ea9ecb7085aab
Carsten Thiel <thiel@sub.uni-goettingen.de>
2016-07-08 11:41:11 +0200

    adding license

--- static/js/mystuff.js
+++ static/js/mystuff.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 SUB Goettingen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 jQuery(document).ready(function($) {
 
   for (var currlist of ['white','black','gray']) {
@@ -19,7 +35,7 @@ jQuery(document).ready(function($) {
     var result = '';
     var atsymb = input.indexOf("@");
     if (input == '') { $('#pruefModalAlert').text('Keine Eingabe.')}
-    else { 
+    else {
       if (atsymb < 0) { $('#pruefModalAlert').text('Kein @ enthalten.')}
       else {
         queryDomain = input.substring(atsymb);
@@ -51,7 +67,7 @@ jQuery(document).ready(function($) {
               result = 'Domain ' + queryDomain + ' unbekannt!';
           }
           $('#pruefModalAlert').text(result);
-        }); 
+        });
       }
     }
   });
